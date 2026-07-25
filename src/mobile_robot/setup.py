2460daccf5d4bd78d737fe2b42ae9535533a4a8f
Siya Lalwani <siyalalwani.dcs@gmail.com>
2026-07-25 19:32:23 +0530

    Add continuous wheel joints

--- src/mobile_robot/setup.py
+++ src/mobile_robot/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+from glob import glob
+import os
 package_name = 'mobile_robot'
 
 setup(
@@ -10,13 +11,17 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share',package_name,'launch'),
+         glob('launch/*.py')),
+        (os.path.join('share',package_name,'model'),
+                 glob('model/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='siya',
     maintainer_email='siya@todo.todo',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Mobile Robot using ROS2 Humble',
+    license='Apache-2.0',
     extras_require={
         'test': [
             'pytest',
